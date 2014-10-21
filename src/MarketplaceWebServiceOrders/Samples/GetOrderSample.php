b89bef42237dbdfb5c1b3591ef9ed8a3cc6618ed
Dmitry <kislicynda@gmail.com>
2014-10-21 16:19:32 +0700

    remove require_once

--- src/MarketplaceWebServiceOrders/Samples/GetOrderSample.php
+++ src/MarketplaceWebServiceOrders/Samples/GetOrderSample.php
@@ -21,7 +21,7 @@
  * Get Order Sample
  */
 
-require_once('.config.inc.php');
+// require_once('.config.inc.php');
 
 /************************************************************************
  * Instantiate Implementation of MarketplaceWebServiceOrders
--- src/MarketplaceWebServiceOrders/Samples/ListOrderItemsByNextTokenSample.php
+++ src/MarketplaceWebServiceOrders/Samples/ListOrderItemsByNextTokenSample.php
@@ -21,7 +21,7 @@
  * List Order Items By Next Token Sample
  */
 
-require_once('.config.inc.php');
+// require_once('.config.inc.php');
 
 /************************************************************************
  * Instantiate Implementation of MarketplaceWebServiceOrders
